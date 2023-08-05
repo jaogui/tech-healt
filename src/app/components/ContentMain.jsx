6ed1670f21ba13142f148c372209b25f8bb78013
João Guilherme <joaobenine@gmail.com>
2023-08-04 23:12:28 -0300

    Sessão dashboard lembretes e consultas

--- src/app/components/ContentMain.jsx
+++ src/app/components/ContentMain.jsx
@@ -1,6 +1,7 @@
 import { React, useContext } from "react";
 import { contextApp } from "../ContextApp";
-import DashboardBlock from "../components/DashboardBlock"
+import DashboardBlock from "../components/DashboardBlock";
+import Notice from "../components/Notice";
 
 function ContentMain() {
   const { contentView } = useContext(contextApp);
@@ -12,31 +13,87 @@ function ContentMain() {
           <div className="">
             <h3 className="text-2xl">Dashboard Info</h3>
             <div className="flex gap-4 py-2 flex-wrap">
-                <DashboardBlock 
-                  title="Agendamentos do dia"
-                  infoContentMain="50"
-                  infoContentSecondary="Agendados"
-                  description="Verifique seus agendamentos do dia"
-                />
-                <DashboardBlock
-                  title="Atendimentos do dia"
-                  infoContentMain="175"
-                  infoContentSecondary="Atendidos"
-                  description="Atendimentos realizados no dia"
-                />
-                <DashboardBlock 
-                  title="Avisos"
-                  infoContentMain="14"
-                  infoContentSecondary="Avisos"
-                  description="Verifique avisos importantes"
-                />
-                <DashboardBlock 
-                  title="Lambretes"
-                  infoContentMain="26"
-                  infoContentSecondary="Lembretes"
-                  description="Lembretes adicionados"
-                />
+              <DashboardBlock
+                title="Agendamentos do dia"
+                infoContentMain="50"
+                infoContentSecondary="Agendados"
+                description="Verifique seus agendamentos do dia"
+              />
+              <DashboardBlock
+                title="Atendimentos do dia"
+                infoContentMain="175"
+                infoContentSecondary="Atendidos"
+                description="Atendimentos realizados no dia"
+              />
+              <DashboardBlock
+                title="Avisos"
+                infoContentMain="14"
+                infoContentSecondary="Avisos"
+                description="Verifique avisos importantes"
+              />
+              <DashboardBlock
+                title="Lambretes"
+                infoContentMain="26"
+                infoContentSecondary="Lembretes"
+                description="Lembretes adicionados"
+              />
             </div>
+          </div>
+
+          <div className="flex w-full gap-2">
+            <div className="grid grid-flow-col max-w-[700px] gap-4 w-full items-center">
+              <div className="w-full">
+                <h3 className="text-2xl py-3">Avisos</h3>
+                <div className="flex flex-col gap-2 w-full ">
+                  <Notice
+                    priority={`#de6767b5`}
+                    titleNotice="Aviso de consulta"
+                    idCheckbox="warningEdit"
+                    descriptionNotice="Luciano solicitou consulta para terça-feira."
+                  />
+                  <Notice
+                    titleNotice="Aviso de consulta"
+                    idCheckbox="warningConfirm"
+                    descriptionNotice="Luciana confirmou horário."
+                  />
+                  <Notice
+                    titleNotice="Aviso de consulta"
+                    idCheckbox="warningCancel"
+                    descriptionNotice="Vivian cancelou consulta."
+                  />
+                </div>
+              </div>
+              <div>
+                <h3 className="text-2xl py-3">Lembretes</h3>
+                <div className="flex flex-col w-full gap-2">
+                  <Notice
+                    titleNotice="Lembrete de consulta"
+                    idCheckbox="reminderQuery"
+                    descriptionNotice="Consulta hoje ás 15:00 com Luciana."
+                  />
+                  <Notice
+                    priority={`#de6767b5`}
+                    titleNotice="Lembrete de consulta"
+                    idCheckbox="reminderInfo"
+                    descriptionNotice="Remarcar consulta com Vivian."
+                  />
+                  <Notice
+                    priority={`#67c8deb5`}
+                    titleNotice="Lembrete de consulta"
+                    idCheckbox="reminderConfirm"
+                    descriptionNotice="Confirmar consulta do Luciano."
+                  />
+                </div>
+              </div>
+            </div>
+
+            {/* <div className="">
+              <h3 className="text-2xl">Agendamentos</h3>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti
+              fuga consectetur recusandae? Quis accusantium, dicta assumenda et
+              reprehenderit aut adipisci, nihil ullam fuga aspernatur minima
+              voluptates, asperiores itaque. Similique, vel?
+            </div> */}
           </div>
         </section>
       ) : null}
---
+++ src/app/components/Notice.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+function Notice({ titleNotice, descriptionNotice, idCheckbox, priority }) {
+  const styleNotices ={
+    background: priority
+  }
+
+
+  return (
+    <label
+      htmlFor={idCheckbox}
+      style={styleNotices}
+      className="w-full border text-sm rounded-sm bg-slate-100 p-2 border-slate-200 flex gap-4"
+    >
+      <input type="checkbox" id={idCheckbox} />
+      <div className="flex flex-col text-sm">
+        <p className="text-slate-800 font-semibold">{titleNotice}</p>
+        <p className="">{descriptionNotice}</p>
+      </div>
+    </label>
+  );
+}
+
+export default Notice;
