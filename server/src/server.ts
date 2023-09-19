7b65589420b5042ec4f136d30ffa8b9db9a5389a
João Guilherme <joaobenine@gmail.com>
2023-09-19 16:40:34 -0300

    fastify/cors p/ especificar acesso ao front-end

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,9 +1,12 @@
 import { fastify } from 'fastify'
+import { fastifyCors } from '@fastify/cors'
 import { getAllNotifications } from './routes/get-notification'
 import { newAppointment } from './routes/new-appointment'
 
 const app = fastify()
-
+app.register(fastifyCors, {
+  origin: '*',
+})
 app.register(getAllNotifications)
 app.register(newAppointment)
 
