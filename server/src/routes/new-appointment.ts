ca453b45386daabf38109ed08d40b605d98ae4a1
João Guilherme <joaobenine@gmail.com>
2023-09-25 17:19:32 -0300

    criando campo p/ data no bd

--- server/src/routes/new-appointment.ts
+++ server/src/routes/new-appointment.ts
@@ -6,17 +6,19 @@ interface AppointmentData{
   nameClient: string;
   idUser: string;
   description: string;
+  dateAppointment:  string;
 }
 export async function newAppointment(app: FastifyInstance){
   
   app.post('/appointment', async (request, reply) => {
-    const { nameClient, idUser, description } = request.body as AppointmentData;
+    const { nameClient, idUser, description, dateAppointment } = request.body as AppointmentData;
 
     const newAppointment = await prisma.appointment.create({
       data:{
         nameClient,
         idUser,
         description,
+        dateAppointment
       }
     })
     return newAppointment
