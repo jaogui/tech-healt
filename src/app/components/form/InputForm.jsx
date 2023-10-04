6c57303b033dabf2a6bbf43cc23f69a64663a45c
jbg <joaobenine@gmail.com>
2023-10-04 19:06:15 -0300

    transformando input text p/ formularios em componente

---
+++ src/app/components/form/InputForm.jsx
@@ -0,0 +1,17 @@
+export function InputForm({ htmlForLabel, labelInput, nameInput, idInput, onChange, placeHolder, valueInput }) {
+  return (
+    <label htmlFor={htmlForLabel} className="flex flex-col gap-1 text-sm py-2">
+      {labelInput}
+      <input
+        type="text"
+        name={nameInput}
+        id={idInput}
+        required
+        className="bg-slate-200 py-1 px-2 rounded-sm focus:bg-white"
+        onChange={onChange}
+        placeholder={placeHolder}
+        value={valueInput}
+      />
+    </label>
+  );
+}
